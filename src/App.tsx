9f9192598cc16e84d060a90cdd7cca24601cdc25
AbdulWaheed73 <aw736024@gmail.com>
2025-07-24 02:19:29 +0500

    look changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-
-
 function App() {
   return (
     <div>
-      <video width="640" height="360" controls>
+      <video 
+        width="640" 
+        height="360" 
+        autoPlay 
+        loop 
+        muted
+        playsInline
+      >
         <source src="/vedio.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
